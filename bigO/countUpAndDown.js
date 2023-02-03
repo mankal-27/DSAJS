5c1ddc420af996a77ae38832365d0dabbc215090
mankal-27 <mnkalkutagi@gmail.com>
2023-02-03 17:48:42 +0530

    pairs

--- bigO/countUpAndDown.js
+++ bigO/countUpAndDown.js
@@ -11,3 +11,14 @@ function countUpAndDown(n){
 }
 
 countUpAndDown(10)
+
+
+function printAllPairs(n){
+    for(var i = 0 ; i< n; i++){
+        for(var j = 0 ; j < n ;  j++){
+            console.log(i, j)
+        }
+    }
+}
+
+printAllPairs(12)
